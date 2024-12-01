4d94dfe459130cfa065a9c59faf2ba74612401a7
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2024-12-01 13:04:22 +0200

    add pagination, remove ?page from url

--- app/Livewire/Products.php
+++ app/Livewire/Products.php
@@ -5,13 +5,18 @@ namespace App\Livewire;
 use App\Models\Product;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
 
 class Products extends Component
 {
+    use WithPagination;
+    use WithoutUrlPagination;
+
     public function render(): View
     {
         return view('livewire.products', [
-            'products' => Product::all(),
+            'products' => Product::query()->paginate(10),
         ]);
     }
 }
