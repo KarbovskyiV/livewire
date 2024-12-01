d7579e424b365622a31283f171eb545be8f26530
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2024-12-01 14:46:12 +0200

    add searching in table

--- app/Livewire/Products.php
+++ app/Livewire/Products.php
@@ -2,8 +2,11 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
 use Livewire\Component;
 use Livewire\WithoutUrlPagination;
 use Livewire\WithPagination;
@@ -13,13 +16,31 @@ class Products extends Component
     use WithPagination;
     use WithoutUrlPagination;
 
+    public Collection $categories;
+    public string $searchQuery = '';
+    public int $searchCategory = 0;
+
+    public function mount(): void
+    {
+        $this->categories = Category::query()->pluck('name', 'id');
+    }
+
     public function render(): View
     {
         $products = Product::with('category')
+            ->when($this->searchQuery !== '', fn(Builder $query) => $query->where('name', 'like', '%'. $this->searchQuery .'%'))
+            ->when($this->searchCategory > 0, fn(Builder $query) => $query->where('category_id', $this->searchCategory))
             ->paginate(10);
 
         return view('livewire.products', [
             'products' => $products,
         ]);
+    }
+
+    public function updating(string $key): void
+    {
+        if ($key === 'searchQuery' || $key === 'searchCategory') {
+            $this->resetPage();
+        }
     }
 }
