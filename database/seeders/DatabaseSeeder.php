1feca2114e09f522bb80cb56d5106311bcc154c9
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2025-01-05 13:44:00 +0200

    create Country and City entity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Country;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 
@@ -21,5 +22,15 @@ class DatabaseSeeder extends Seeder
             ->each(function (Product $product) use ($categories) {
                 $product->categories()->sync($categories->random(2));
             });
+
+        $country = Country::query()->create(['name' => 'United Kingdom']);
+        $country->cities()->create(['name' => 'London']);
+        $country->cities()->create(['name' => 'Manchester']);
+        $country->cities()->create(['name' => 'Liverpool']);
+
+        $country = Country::query()->create(['name' => 'United States']);
+        $country->cities()->create(['name' => 'Washington']);
+        $country->cities()->create(['name' => 'New York City']);
+        $country->cities()->create(['name' => 'Denver']);
     }
 }
